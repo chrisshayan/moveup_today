aa1df154b33831326b430b24d98effc75116e891
hamedshayan@gmail.com <Hamed9243>
2015-05-05 17:47:58 +0700

    Added Error Handling

--- client/templates/signin/signin_layout.js
+++ client/templates/signin/signin_layout.js
@@ -4,6 +4,11 @@ Template.signinLayout.events({
 
         Meteor.call("login", event.target.username.value, event.target.password.value, function (error, result) {
             console.log(result);
+            if(result.status == 200) {
+
+            } else {
+                throwError("Username or password is incorrect. Please ensure your account is also activated.");
+            }
         });
     }
 });
