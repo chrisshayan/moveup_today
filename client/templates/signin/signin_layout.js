1ce1538ce0eface59377392a6dc816918681552f
Ngo Anh Tuan <Tuan.Ngo@navigosgroup.com>
2015-05-06 14:42:09 +0700

    keep user information to session after login

--- client/templates/signin/signin_layout.js
+++ client/templates/signin/signin_layout.js
@@ -1,16 +1,19 @@
 Template.signinLayout.events({
     'submit form': function(event, template) {
         event.preventDefault();
-
         clearErrors();
         Meteor.call("vietnamworksLogin", event.target.username.value, event.target.password.value, function (error, result) {
-            console.log(result);
+        console.log(result);
             if(result.status == 200) {
+                //save information to session
+                Session.set('user', result);
                 if(result.userHasMatchingScore) {
 
                 } else {
                     throwError("Your account does not have enough information for Matching Score. Update in <a href='http://www.vietnamworks.com/my-career-center'>here.</a>");
                 }
+                //go to dashboard page
+                Router.go('home_layout');
             } else {
                 throwError("Username or password is incorrect. Please ensure your account is also activated.");
             }
