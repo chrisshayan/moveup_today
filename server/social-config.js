94c9fba668a81c112946942f9a43ea31ec8d0f0a
hamedshayan@gmail.com <Hamed9243>
2015-05-07 20:58:02 +0700

    Social Sign In

---
+++ server/social-config.js
@@ -0,0 +1,19 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '356907771182860',
+    secret: 'eec95a3ad858fc9680844cf93b9e7341'
+});
+
+ServiceConfiguration.configurations.remove({
+    service: 'linkedin'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'linkedin',
+    clientId: '75m9hpcac3e1q3',
+    secret: 'OhKQ2KF5I8zUubol'
+});
