1128ab06d157cad32d17906ac583ebe03a8c4d22
Son Tran <son.tran@navigosgroup.com>
2015-05-07 22:17:11 +0700

    Update sharing facebook button

--- client/templates/home/jumbotron.js
+++ client/templates/home/jumbotron.js
@@ -10,8 +10,8 @@ Template.jumbotron.helpers({
   shareData: function() {
       return {
           title: 'Move Up To Day - Kinh nghiệm tối ưu cho người tìm việc',
-          summary: 'Move Up To Day - Kinh nghiệm tối ưu cho người tìm việc - Được phát triển bởi VietnamWorks',
-          url: 'http://moveup.today.com',
+          summary: 'Move Up To Day. Kinh nghiệm tối ưu cho người tìm việc. Được phát triển bởi VietnamWorks. Real-Time Job Matching cung cấp các cơ hội việc làm phù hợp nhất mà bạn không cần phải tìm kiếm',
+          url: 'http://moveup.today',
           image: 'http://images.vietnamworks.com/img/thumbnail.png'
       }
   }
