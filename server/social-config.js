12e63f26bce8cdfc4b7f413f5749ae92f334784c
Ngo Anh Tuan <Tuan.Ngo@navigosgroup.com>
2015-05-13 13:59:41 +0700

    refactor get linkedIn from settings.json

--- server/social-config.js
+++ server/social-config.js
@@ -1,19 +1,9 @@
 ServiceConfiguration.configurations.remove({
-    service: 'facebook'
+    service: Meteor.settings.private.socialProviders.linkedIn.name
 });
 
 ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '356907771182860',
-    secret: 'eec95a3ad858fc9680844cf93b9e7341'
-});
-
-ServiceConfiguration.configurations.remove({
-    service: 'linkedin'
-});
-
-ServiceConfiguration.configurations.insert({
-    service: 'linkedin',
-    clientId: '75m9hpcac3e1q3',
-    secret: 'OhKQ2KF5I8zUubol'
+    service: Meteor.settings.private.socialProviders.linkedIn.name,
+    clientId:  Meteor.settings.private.socialProviders.linkedIn.clientId,
+    secret: Meteor.settings.private.socialProviders.linkedIn.secret
 });
